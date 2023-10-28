1e6c4cd45dc972ee80b9c93e59442a2ca5eab8f3
sandeepgottipati <snchowdary7@gmail.com>
2023-10-27 20:55:10 -0500

    fixed the overflow of the todo list.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,8 +5,18 @@ interface props {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const TodoList: React.FC<props> = ({ todos, setTodos }) => {
+  if (todos.length === 0) {
+    return (
+      <div className="flex  bg-white items-center p-4 rounded-lg">
+        <h2 className="text-3xl font-semibold ">
+          No Task Left!! Go ahead and add some more tasks.
+        </h2>
+      </div>
+    );
+  }
+
   return (
-    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 items-center bg-white rounded-lg shadow-xl w-[90%] md:max-w-2xl p-10">
+    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 items-center  overflow-y-scroll   bg-white rounded-lg shadow-xl w-[90%] md:max-w-2xl p-10">
       {todos.map((todo) => {
         return (
           <TodoItem
